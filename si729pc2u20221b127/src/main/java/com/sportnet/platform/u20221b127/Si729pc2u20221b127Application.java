db0deb558ed3a45153a6912d632b93928b36fb4f
salvadoorssalinas <u20221b127@upc.edu.pe>
2024-06-13 21:06:01 -0500

    chore: create project

---
+++ si729pc2u20221b127/src/main/java/com/sportnet/platform/u20221b127/Si729pc2u20221b127Application.java
@@ -0,0 +1,13 @@
+package com.sportnet.platform.u20221b127;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Si729pc2u20221b127Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Si729pc2u20221b127Application.class, args);
+    }
+
+}
---
+++ si729pc2u20221b127/src/main/java/com/sportnet/platform/u20221b127/shared/interfaces/rest/resources/MessageResource.java
@@ -0,0 +1,4 @@
+package com.sportnet.platform.u20221b127.shared.interfaces.rest.resources;
+
+public record MessageResource(String message) {
+}
